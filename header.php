71fcfbb75065cd5d35d927fec77d1acd711c896b
alnashief99 <alnashief99@users.noreply.github.com>
2021-03-01 02:57:46 +0800

    update

--- header.php
+++ header.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>
 		Nash's Portfolio
 	</title>
 </head>
 <link href="style/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-	<div class="container">
+	<div class="container-fluid">
 	<body style="background-image: url('img/bgi.jpg'); background-repeat: no-repeat;
   		background-attachment: fixed;
   		background-position: center;
   		background-size: 100%;">
-		
+
+
+		<nav class="navbar navbar-dark bg-dark">
 			<a href="https://www.facebook.com/xLuckyNashx/" class="btn btn-primary" target="_blank">
                 <svg  width="20" height="20" fill="currentColor" class="bi bi-facebook" viewBox="0 0 20 20">
   					<path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"></path>
 				</svg>
 				Fb
             </a>
-			
+		</nav>
