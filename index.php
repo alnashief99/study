5a2bd65fc46dd947653f297db3716cc32bf1a70b
alnashief99 <alnashief99@users.noreply.github.com>
2021-03-01 07:35:07 +0800

    added github icon and my picture

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php include 'header.php'; ?>
 
-<h1 style="text-align: center; color: darkgreen; font-family: calibre">Nash's Portfolio</h1>
+<h1 align=center>
+    <svg height="500" width="400" >
+    <defs>
+        <clipPath id="myCircle">
+    <circle cx="250" cy="145" r="125" stroke="yellow" stroke-width="10" fill="black">
+    </defs>
+    <image width="500" height="335" href="img/nash.jpg" clip-path="url(#myCircle)" />
+      </svg>
+    </svg>
+</h1>
 
 
 <?php include 'footer.php'; ?>
