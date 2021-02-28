cd7f47290d42de5ca2b72b2cf398058f9d40ccc1
alnashief99 <alnashief99@users.noreply.github.com>
2021-03-01 00:27:18 +0800

    put facebook button

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include 'header.php'; ?>
 
-<h1 style="text-align: center; color: darkgreen; font-family: calibre">this is a text</h1>
+<h1 style="text-align: center; color: darkgreen; font-family: calibre">Nash's Portfolio</h1>
 
 
 <?php include 'footer.php'; ?>
