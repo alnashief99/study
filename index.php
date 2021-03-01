0423117dccedd38bc0862009208c31910e848722
alnashief99 <alnashief99@users.noreply.github.com>
2021-03-02 02:36:20 +0800

    update index

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php include 'header.php'; ?>
-
-<h1 style="margin-left: 40%; margin-top: -8%; position: absolute; ">
+<div cx="100" cy="200" r="120" stroke="yellow" stroke-width="10" fill="black">
+<h1 style="margin-left: 40%; margin-top: -8%; position: absolute;" >
+    
     <svg height="400px" width="250" >
     <defs>
-        <clipPath id="myCircle">
-    <circle cx="100" cy="200" r="100" stroke="yellow" stroke-width="10" fill="black">
+        <clipPath  id="myCircle">
+    <circle  position  cx="100" cy="200" r="100" stroke="yellow" stroke-width="30" fill="black">
+    </circle>
     </defs>
-    <image  height="400px" width="250" href="img/nash.jpg" clip-path="url(#myCircle)" />
+    <image style="z-index: -1"  height="400px" width="250" href="img/nash.jpg" clip-path="url(#myCircle)" />
       </svg>
 </h1>
+</div>
 <h5 class="name" style="margin-top: 13%; padding-left:39%; color: white; font-family:monospace " > Mohammad, Al-Nashief S.</h5>
 
 <?php include 'footer.php'; ?>
