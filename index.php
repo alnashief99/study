303af6beda41f4eac3aa60e0008e1e62e97db30f
alnashief99 <alnashief99@users.noreply.github.com>
2021-03-04 10:12:49 +0800

    update
    add ring in profile pic

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <?php include 'header.php'; ?>
-<div cx="100" cy="200" r="120" stroke="yellow" stroke-width="10" fill="black">
-<h1 class="picture" >
+
+<div .class="frame">
     
-    <svg height="400px" width="250" >
-    <defs>
-        <clipPath  id="myCircle">
-    <circle  position  cx="100" cy="200" r="100" stroke="yellow" stroke-width="30" fill="black">
-    </circle>
-    </defs>
-    <image style="z-index: -1"  height="400px" width="250" href="img/nash.jpg" clip-path="url(#myCircle)" />
-      </svg>
-</h1>
+            <image class="pics"  src="img/nash.jpg" >
+
 </div>
-<h5 align="center" class="name" style="margin-top: 13%; color: white; font-family:monospace " > Mohammad, Al-Nashief S.</h5>
+<h5 class="nash"> 
+
+	Mohammad, Al-Nashief Sakaluran
+
+</h5>
+
+
+
+
 
 <?php include 'footer.php'; ?>
